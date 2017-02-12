5b124665d55ebf5395d6d6a618acaca0d490c582
i05nagai <i05nagai@gmail.com>
2017-02-13 00:50:10 +0900

    Add greedy_scheduling

--- cmake/chap2.cmake
+++ cmake/chap2.cmake
@@ -3,6 +3,7 @@ LIST (APPEND chap2_SOURCES
     src/chap2/depth_first_search.cc
     src/chap2/factorial.cc
     src/chap2/greedy_coin.cc
+    src/chap2/greedy_scheduling.cc
     src/chap2/lake_counting.cc
 )
 LIST (APPEND chap2_HEADERS
@@ -10,5 +11,6 @@ LIST (APPEND chap2_HEADERS
     src/chap2/depth_first_search.h
     src/chap2/factorial.h
     src/chap2/greedy_coin.h
+    src/chap2/greedy_scheduling.h
     src/chap2/lake_counting.h
 )
---
+++ src/chap2/greedy_scheduling.h
@@ -0,0 +1,6 @@
+#pragma once
+
+int greedy_scheduling(
+    const int numJob, const int start[], const int end[]);
+int greedy_scheduling1(
+    const int numJob, const int start[], const int end[]);
