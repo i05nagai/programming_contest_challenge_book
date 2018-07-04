6ec61cd905b5ed0253ed2978e256e86141014b83
i05nagai <i05nagai@gmail.com>
2018-07-04 20:13:10 +0900

    Add cmake configuration for chap3

---
+++ cmake/chap3.cmake
@@ -0,0 +1,30 @@
+LIST (APPEND chap3_SOURCES
+    src/chap3/binary_index_tree.cc
+    src/chap3/binary_search/aggressive_cows.cc
+    src/chap3/binary_search/cable_master.cc
+    src/chap3/binary_search/lower_bound.cc
+    src/chap3/coordinate_compress.cc
+    src/chap3/crane.cc
+    src/chap3/range_minimum_query.cc
+    src/chap3/two_pointer_technique/face_the_right_way.cc
+    src/chap3/two_pointer_technique/fliptile.cc
+    src/chap3/two_pointer_technique/four_values_whose_sum_is_0.cc
+    src/chap3/two_pointer_technique/jessica_reading_problem.cc
+    src/chap3/two_pointer_technique/physics_experiment.cc
+    src/chap3/two_pointer_technique/subsequence.cc
+)
+LIST (APPEND chap3_HEADERS
+    src/chap3/binary_index_tree.h
+    src/chap3/binary_search/aggressive_cows.h
+    src/chap3/binary_search/cable_master.h
+    src/chap3/binary_search/lower_bound.h
+    src/chap3/coordinate_compress.h
+    src/chap3/crane.h
+    src/chap3/range_minimum_query.h
+    src/chap3/two_pointer_technique/face_the_right_way.h
+    src/chap3/two_pointer_technique/fliptile.h
+    src/chap3/two_pointer_technique/four_values_whose_sum_is_0.h
+    src/chap3/two_pointer_technique/jessica_reading_problem.h
+    src/chap3/two_pointer_technique/physics_experiment.h
+    src/chap3/two_pointer_technique/subsequence.h
+)
