e9ab240636a569e45a941c8559e0ba86014f7c4d
i05nagai <i05nagai@gmail.com>
2018-09-22 14:07:49 +0900

    Update prim

--- src/algorithm/graph/minimum_spanning_tree/prim.cc
+++ src/algorithm/graph/minimum_spanning_tree/prim.cc
@@ -1,35 +1,86 @@
 #include "algorithm/graph/minimum_spanning_tree/prim.h"
 #include <queue>
+#include <utility>
+#include <iostream>
 
 namespace algorithm { namespace graph {
-int prim(int num_vertex, int** cost, int mincost[]) {
-  // cost[i][j]: cost of edge from i to j
-
-  // included by the minimum spanning tree
+int prim(int num_vertex, int** cost) {
+  // mincost[v]: the minimum cost of path between X and v where X is the set of vertex
+  int mincost[num_vertex];
+  // used[v]: v is contained in X if true, 
   bool used[num_vertex];
-  std::fill(used, used + num_vertex, false)
-  std::priority_queue<int> qs;
-  // Pair(cost, vertex)
-  typedef Pair std::pair<int, int>;
-
-  int total_cost = 0;
-  qs.push(Pair(0, 0));
   for (int i = 0; i < num_vertex; ++i) {
-    const Pair q = qs.top();
-    qs.pop();
-    const int v = q.second;
-
+    mincost[i] = PRIM_INF;
+    used[i] = false;
+  }
+  mincost[0] = 0;
+  // start from 0
+  int v = 0;
+  int total_cost = 0;
+  while (true) {
+    // calculate cost
+    for (int u = 0; u < num_vertex; ++u) {
+      mincost[u] = std::min(mincost[u], cost[v][u]);
+    }
+    total_cost += mincost[v];
     used[v] = true;
-    for (int vv = 0; vv < num_vertex; ++vv) {
-      if (!used[vv] && cost[v][vv] != PRIM_INF) {
-        qs.push(Pari(cost[v][vv], vv));
+
+    v = -1;
+    // find a edge whose cost is the minimum in vertice from used vertice
+    for (int u = 0; u < num_vertex; ++u) {
+      // min cost of u is not determined 
+      // and (not found edge or cost of edge is less than)
+      if (!used[u]
+          && (v == -1 || mincost[u] < mincost[v])) {
+        v = u;
       }
     }
-    // add cost
-    const int c = q.first;
-    total_cost += c;
+    // no update
+    if (v == -1) {
+      break;
+    }
   }
-
   return total_cost;
 }
+/*
+int prim(int num_vertex, int** cost) {
+  // mincost[v]: the minimum cost of path between X and v where X is the set of vertex
+  int mincost[num_vertex];
+  // used[v]: v is contained in X if true, 
+  bool used[num_vertex];
+  for (int i = 0; i < num_vertex; ++i) {
+    mincost[i] = PRIM_INF;
+    used[i] = false;
+  }
+  // start from 0
+  mincost[0] = 0;
+  int total_cost = 0;
+  while (true) {
+    int v = -1;
+    // find a edge whose cost is the minimum in vertice
+    
+    for (int u = 0; u < num_vertex; ++u) {
+      // min cost of u is not determined 
+      // and (not found edge or cost of edge is less than)
+      if (!used[u]
+          && (v == -1 || mincost[u] < mincost[v])) {
+        v = u;
+      }
+    }
+    // not update
+    if (v == -1) {
+      break;
+    }
+
+    used[v] = true;
+    total_cost += mincost[v];
+    for (int u = 0; u < num_vertex; ++u) {
+      mincost[u] = std::min(mincost[u], cost[v][u]);
+      std::cout << "mincost[u]: " << mincost[u] << std::endl;
+    }
+    std::cout << std::endl;
+  }
+  return total_cost;
+}
+*/
 } } // namespace algorithm { namespace graph {
--- src/algorithm/graph/minimum_spanning_tree/prim.h
+++ src/algorithm/graph/minimum_spanning_tree/prim.h
@@ -1,10 +1,17 @@
 #pragma once
-#include "algorithm/graph/prim/prim.h"
 
 namespace algorithm {
 namespace graph {
-constexpr int PRIM_INF = 100000;
-int prim(int num_vertex, int**cost, int mincost[]);
+  constexpr int PRIM_INF = 100000;
+  /**
+   * @brief 
+   *
+   * @param num_vertex
+   * @param cost cost[i][j]: cost of edge from i to j
+   *
+   * @return 
+   */
+  int prim(int num_vertex, int** cost);
 } // namespace graph
 } // namespace algorithm
 
