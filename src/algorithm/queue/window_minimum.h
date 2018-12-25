9d3f6b701310730effd1dcf905e231baa51e2ac5
i05nagai <i05nagai@gmail.com>
2018-12-25 19:48:09 +0900

    Add queue/window_minimum

---
+++ src/algorithm/queue/window_minimum.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "algorithm/graph/graph.h"
+
+namespace algorithm {
+namespace queue {
+  std::vector<int> SolveWindowMinimum(
+      int sequence[],
+      const int sequence_size,
+      const int window_size);
+} // namespace queue
+} // namespace algorithm
+
